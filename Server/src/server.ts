2224393bc613f444b66ed21486a23e12bb0d864c
Vishal Kumar <mail.vishalkushwaha@gmail.com>
2026-04-10 13:10:59 +0530

    basic layout of authentication is compleated

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -1,6 +1,12 @@
 import express from "express"
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./utils/auth.js";
 
 const app = express()
+
+app.all("/api/auth/*", toNodeHandler(auth))
+
+app.use(express.json())
 
 app.get('/', (req, res)=>{
   res.send('Server is running fine on website')
